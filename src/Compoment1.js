60646aaf297f263a8b9cbcd8f4496ede68e58d26
Rama Chandra Viswanadh <ramachandraviswanadh333@gmail.com>
2022-11-14 14:01:01 +0530

    components learned

---
+++ src/Compoment1.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Compoment1() {
+  return (
+    <div className='component1'>
+        <h1>part1</h1>
+      <p>lorem50thtr garhe erherh rfhrh rhhy rferher haehr erhh treherh therh rherh htrerh ehehe etrheh aeh the hetr haetrhaetrh hheh eh eth th th tht ht h th t ht
+      </p>
+      <img src="https://cdn.pixabay.com/photo/2021/08/04/13/06/software-developer-6521720__340.jpg" alt='imageof coding'/>
+    </div>
+  )
+}
+
+export default Compoment1
---
+++ src/Component2.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Component2() {
+  return (
+    <div className='component2'>
+        <h1 >part2</h1>
+      <p>lorem50thtr garhe erherh rfhrh rhhy rferher haehr erhh treherh therh rherh htrerh ehehe etrheh aeh the hetr haetrhaetrh hheh eh eth th th tht ht h th t ht
+      </p>
+      <img src="https://cdn.pixabay.com/photo/2021/08/04/13/06/software-developer-6521720__340.jpg" alt='imageof coding'/>
+    </div>
+  )
+}
+
+export default Component2
